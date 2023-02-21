ba48b67e44de29bd47262c3fa4dceaabda8c386b
Hophélie Bourgeois <hophelie@twiiky-corp.fr>
2023-02-21 17:36:09 +0100

    Todo

--- src/App.svelte
+++ src/App.svelte
@@ -7,94 +7,93 @@
 	// import FaBeer from 'svelte-icons/fa/FaBeer.svelte'
 	// import FaAccessibleIcon from 'svelte-icons/fa/FaAccessibleIcon.svelte'
 	// COURS 1 ----------------------------------
-  
 	// COURS 2 ----------------------------------
-	// import TodoList from './lib/TodoList.svelte';
-	// import { v4 as uuid } from 'uuid';
-	// import { tick, onMount } from 'svelte';
+	import TodoList from './lib/TodoList.svelte';
+	import { v4 as uuid } from 'uuid';
+	import { tick, onMount } from 'svelte';
 
-	// let todos = null;
-	// let isAdding = false;
-	// let disabledItems = [];
-	// onMount(() => {
-	// 	loadTodos();
-	// });
+	let todos = null;
+	let isAdding = false;
+	let disabledItems = [];
+	onMount(() => {
+		loadTodos();
+	});
 
-	// function loadTodos() {
-	// 	fetch('https://jsonplaceholder.typicode.com/todos?_limit=3').then(async (response) => {
-	// 		if (response.ok) {
-	// 			todos = await response.json();
-	// 		} else {
-	// 			throw new Error('Something went wrong');
-	// 		}
-	// 	});
-	// }
-	// async function handleAddTodo(event) {
-	// 	//  e.preventDefault();
+	function loadTodos() {
+		fetch('https://jsonplaceholder.typicode.com/todos?_limit=3').then(async (response) => {
+			if (response.ok) {
+				todos = await response.json();
+			} else {
+				throw new Error('Something went wrong');
+			}
+		});
+	}
+	async function handleAddTodo(event) {
+		//  e.preventDefault();
 
-	// 	isAdding = true;
-	// 	await fetch('https://jsonplaceholder.typicode.com/todos', {
-	// 		method: 'POST',
-	// 		body: JSON.stringify({
-	// 			title: event.detail.title,
-	// 			completed: false
-	// 		}),
-	// 		headers: {
-	// 			'Content-type': 'application/json; charset=UTF-8'
-	// 		}
-	// 	}).then(async (response) => {
-	// 		if (response.ok) {
-	// 			const todo = await response.json();
-	// 			console.log(todo);
-	// 			todos = [...todos, { ...todo, id: uuid() }];
-	// 		}
-	// 	});
-	// 	isAdding = false;
-	// }
-	// async function handleRemoveTodo(event) {
-	// 	let id = event.detail.id;
-	// 	if (disabledItems.includes(id)) return;
-	// 	disabledItems = [...disabledItems, id];
-	// 	await fetch(`https://jsonplaceholder.typicode.com/todos/${id}`, {
-	// 		method: 'DELETE'
-	// 	}).then((response) => {
-	// 		if (response.ok) {
-	// 			todos = todos.filter((e) => e.id !== id);
-	// 		} else {
-	// 			alert('erreur');
-	// 		}
-	// 	});
-	// 	disabledItems = disabledItems.filter((itemId) => itemId !== id);
-	// }
-	// async function handleToggleTodo(event) {
+		isAdding = true;
+		await fetch('https://jsonplaceholder.typicode.com/todos', {
+			method: 'POST',
+			body: JSON.stringify({
+				title: event.detail.title,
+				completed: false
+			}),
+			headers: {
+				'Content-type': 'application/json; charset=UTF-8'
+			}
+		}).then(async (response) => {
+			if (response.ok) {
+				const todo = await response.json();
+				console.log(todo);
+				todos = [...todos, { ...todo, id: uuid() }];
+			}
+		});
+		isAdding = false;
+	}
+	async function handleRemoveTodo(event) {
+		let id = event.detail.id;
+		if (disabledItems.includes(id)) return;
+		disabledItems = [...disabledItems, id];
+		await fetch(`https://jsonplaceholder.typicode.com/todos/${id}`, {
+			method: 'DELETE'
+		}).then((response) => {
+			if (response.ok) {
+				todos = todos.filter((e) => e.id !== id);
+			} else {
+				alert('erreur');
+			}
+		});
+		disabledItems = disabledItems.filter((itemId) => itemId !== id);
+	}
+	async function handleToggleTodo(event) {
 
-  //   let id = event.detail.id;
-  //   if (disabledItems.includes(id)) return;
-	// 	disabledItems = [...disabledItems, id];
+    let id = event.detail.id;
+    if (disabledItems.includes(id)) return;
+		disabledItems = [...disabledItems, id];
 
-	// 	await fetch(`https://jsonplaceholder.typicode.com/todos/${id}`, {
-	// 		method: 'PATCH',
-  //     body: JSON.stringify({
-  //       completed: event.detail.completed
-  //     }),
-  //     headers:{
-  //       'content-type': 'application/json; charset=UTF-8'
-  //     }
-	// 	}).then((response) => {
-	// 		if (response.ok) {
-	// 			todos = todos.map((todo) => {
-	// 				if (todo.id === id) {
-  //           console.log(response);
-	// 					return { ...todo, completed: event.detail.completed };
-	// 				}
-	// 				return { ...todo };
-	// 			});
-	// 		} else {
-	// 			alert('erreur');
-	// 		}
-	// 	});
-  //   disabledItems = disabledItems.filter((itemId) => itemId !== id);
-	// }
+		await fetch(`https://jsonplaceholder.typicode.com/todos/${id}`, {
+			method: 'PATCH',
+      body: JSON.stringify({
+        completed: event.detail.completed
+      }),
+      headers:{
+        'content-type': 'application/json; charset=UTF-8'
+      }
+		}).then((response) => {
+			if (response.ok) {
+				todos = todos.map((todo) => {
+					if (todo.id === id) {
+            console.log(response);
+						return { ...todo, completed: event.detail.completed };
+					}
+					return { ...todo };
+				});
+			} else {
+				alert('erreur');
+			}
+		});
+    disabledItems = disabledItems.filter((itemId) => itemId !== id);
+	}
 	// COURS 2 ----------------------------------
 </script>
 
@@ -114,7 +113,7 @@
 <!-- COURS 1 ---------------------------------- -->
 <!-- COURS 2 ---------------------------------- -->
 
-<!-- <TodoList
+<TodoList
 	{todos}
 	disabledAdding={isAdding}
 	on:addtodo={handleAddTodo}
@@ -123,7 +122,7 @@
   let:todo
 >
 
-</TodoList> -->
-<!-- COURS 2 ---------------------------------- -->
+</TodoList>
+
 <style>
 </style>
